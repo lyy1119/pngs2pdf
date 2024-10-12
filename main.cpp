299d93ab24983e0ee55ce762d0ffec4e527a36d5
lyy1119 <lyy2286301015@126.com>
2024-10-12 20:48:52 +0800

    基本代码写好

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <filesystem>
 #include <sstream>
 #include <hpdf.h>
+#include <hpdf_objects.h>
 
 int get_page_numbers()
 {
@@ -24,25 +25,52 @@ void error_handler(HPDF_STATUS error_no, HPDF_STATUS detail_no, void *user_data)
     exit(1);
 }
 
-std::vector<std::string> generate_image_file_vector(int page_numbers)
+std::vector<std::string> generate_image_file_vector(int pageNumbers)
 {
-    std::vector<std::string> image_files;
-    for (int i = 1; i <= page_numbers; i++)
+    std::vector<std::string> imageFiles;
+    for (int i = 1; i <= pageNumbers; i++)
     {
-        image_files.push_back(std::to_string(i) + ".png");
+        imageFiles.push_back(std::to_string(i) + ".png");
     }
-    return image_files;
+    return imageFiles;
 }
 
 int main()
 {
     int pageNumber = get_page_numbers();
     std::cout << pageNumber << " pages found." << std::endl;
+    std::vector<std::string> imageFiles;
+    imageFiles = generate_image_file_vector(pageNumber);
     HPDF_Doc pdf = HPDF_New(error_handler, nullptr);
-
     if (!pdf) {
         std::cerr << "Failed to create PDF object" << std::endl;
         return 1;
     }
-
+    try
+    {
+        for (const auto &imageFile : imageFiles)
+        {
+            HPDF_Page page = HPDF_AddPage(pdf);
+            HPDF_Image image = HPDF_LoadPngImageFromFile(pdf, imageFile.c_str());
+            if (!image) {
+                HPDF_Free(pdf);
+                throw std::runtime_error("Failed to load PNG image.");
+            }
+            HPDF_REAL imageWidth = HPDF_Image_GetWidth(image);
+            HPDF_REAL imageHeight = HPDF_Image_GetHeight(image);
+            HPDF_Page_SetWidth(page, imageWidth);
+            HPDF_Page_SetHeight(page, imageHeight);
+            HPDF_Page_DrawImage(page, image, 0, 0, imageWidth, imageHeight);
+        }
+        HPDF_SaveToFile(pdf, "output.pdf");
+        HPDF_Free(pdf);
+        std::cout << "PDF successfully created!" << std::endl;
+    }
+    catch(...)
+    {
+        HPDF_Free(pdf);
+        std::cerr << "An error occurred while creating the PDF." << std::endl;
+        return 1;
+    }
+    return 0;
 }
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all:
-	g++ -std=c++17 main.cpp -o pngs2pdf -lhpdf
+	g++ -std=c++17 main.cpp -static -O2 -Wall -o pngs2pdf -lhpdf -lpng -lz -lm -lstdc++
